41228d0566dfe86439d153bd53da3c718dbecc98
Farma <mazlomiarad@gmail.com>
2024-05-27 20:41:06 +0300

    Update PrimarySale.sol

--- PrimarySale.sol
+++ PrimarySale.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
-/// @author thirdweb
+/// @author SonicZorb
 
 import "./interface/IPrimarySale.sol";
 
 /**
  *  @title   Primary Sale
- *  @notice  Thirdweb's `PrimarySale` is a contract extension to be used with any base contract. It exposes functions for setting and reading
+ *  @notice  SonicZorb's `PrimarySale` is a contract extension to be used with any base contract. It exposes functions for setting and reading
  *           the recipient of primary sales, and lets the inheriting contract perform conditional logic that uses information about
  *           primary sales, if desired.
  */
